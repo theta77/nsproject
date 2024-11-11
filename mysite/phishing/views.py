b83a7c06150f650c303986972d1d699fb20c5118
unknown <springjumpyt@gmail.com>
2024-11-11 14:55:03 +0900

    11-11

--- mysite/phishing/views.py
+++ mysite/phishing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Create your views here.
+def main(request):
+    return render(request, "main.html")
